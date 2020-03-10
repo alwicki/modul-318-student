40290f90b4b4de5cfb513242b9de9cdabdf24d6d
AlWicki <aurellinus_wicki@sluz.ch>
2020-03-10 16:31:11 +0100

    Added TabView
    TabbView for Connections, Stationboard and StationInfo added

--- TransportGUI/MainWindow.xaml.cs
+++ TransportGUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SwissTransport;
 
 namespace TransportGUI
 {
@@ -20,9 +21,61 @@ namespace TransportGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ITransport transport;
         public MainWindow()
         {
             InitializeComponent();
+
+            var datetime = DateTime.Today;
+            DepartureDate.SelectedDate = DateTime.Today;
+            DepartureTime.Text = datetime.ToString("hh:mm");
+
+
+        }
+
+        private void getConnections(object sender, RoutedEventArgs e)
+        {
+            string from = txtFrom.Text.ToString();
+            string to = txtTo.Text.ToString();
+            DateTime selectedDate = (DateTime)DepartureDate.SelectedDate;
+            string date = selectedDate.ToString("yyyy-MM-dd");
+            string time = DepartureTime.Text.ToString();
+            searchConnections(from, to, date, time);
+        }
+
+        private void searchConnections(string from, string to, string date, string time)
+        {
+            //MessageBox.Show(from + to + date + time);
+            transport = new Transport();
+            var connections = transport.GetConnectionsByDateTime(from, to, date, time);
+            ConnectionTable.ItemsSource = connections.ConnectionList;
+        }
+
+        private void getTimeTable(object sender, RoutedEventArgs e)
+        {
+            transport = new Transport();
+            var stationBoard = transport.GetStationBoard("Luzern", "8505000");
+            StationBoardTable.ItemsSource = stationBoard.Entries;
+        }
+
+        private void getStations(object sender, RoutedEventArgs e)
+        {
+            transport = new Transport();
+            if (validateTextfield())
+            {
+                var searchTerm = txtStationInfo.Text.ToString();
+                var stationInfo = transport.GetStations(searchTerm);
+                StationInfoTable.ItemsSource = stationInfo.StationList;
+            }
+        }
+
+        private bool validateTextfield()
+        {
+            if (txtStationInfo.Text.Length > 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
